d1b10810e63f0ff67776e759f964989ba699f14b
Chris Maina <chrismaina@Chris-MacBook.local>
2020-04-11 20:35:40 +0300

    modify response object names

--- app/api/routes/v1/shipments/index.js
+++ app/api/routes/v1/shipments/index.js
@@ -9,14 +9,11 @@ router.get('/shipments', auth.required, async (req, res) => {
     const { user } = req;
     try {
         const shipments = await Shipment.query().where('user_id', user.id).withGraphFetched('cargo');
-        return res.status(200).json({
-            data: shipments
-        });
+        return res.status(200).json({ shipments });
     } catch (error) {
         return res.status(422).json({
-            errors: {
-                message: error.message
-            }
+            message: error.message,
+            error,
         })
     }
 });
@@ -27,7 +24,7 @@ router.post('/shipments', auth.required, async (req, res) => {
         body: { name, mode, type, origin, destination, total, status, cargo }
     } = req;
     try {
-        let shipment = await Shipment.query().findOne({ name });
+        let shipment = await Shipment.query().findOne({ name }).where('user_id', user.id);
         if (shipment) return res.status(409).json({
             errors: {
                 message: `A shipment with name ${name} exists. Use different name`
@@ -59,16 +56,28 @@ router.post('/shipments', auth.required, async (req, res) => {
             )
         );
 
-        return res.status(200).json({
-            data: shipment
-        })
+        return res.status(201).json({ shipment });
     } catch (error) {
         return res.status(422).json({
-            errors: {
-                message: error.message
-            }
+            message: error.message,
+            error,
         })
     }
-})
+});
+
+
+router.patch('/shipments', auth.required, async (req, res) => {
+    const { body: { id } } = req;
+
+    try {
+        const shipment = await Shipment.query().patchAndFetchById(id, req.body);
+        return res.status(200).json({ shipment });
+    } catch (error) {
+        return res.status(422).json({
+            message: error.message,
+            error,
+        })
+    }
+});
 
 module.exports = router;
